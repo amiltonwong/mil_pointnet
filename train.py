d0c09d2d37ee955149140f395cbd06d54a3fe17f
amiltonwong <amiltonwong@gmail.com>
2018-03-27 21:02:17 -0400

    commit

--- train.py
+++ train.py
@@ -252,7 +252,7 @@ def eval_one_epoch(sess, ops, test_writer):
             
     log_string('eval mean loss: %f' % (loss_sum / float(total_seen)))
     log_string('eval accuracy: %f'% (total_correct / float(total_seen)))
-    log_string('eval avg class acc: %f' % (np.mean(np.array(total_correct_class)/np.array(total_seen_class,dtype=np.float))))
+    log_string('eval avg class acc: %f' % (np.mean(np.`(total_correct_class)/np.array(total_seen_class,dtype=np.float))))
          
 
 
